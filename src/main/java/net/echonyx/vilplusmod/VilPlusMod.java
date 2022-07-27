f19269e70b068bf3228f8c28d5cfed926a529f55
Muhammadrizo <yusufovmuhammadrizodev@gmail.com>
2022-07-27 19:11:14 +0500

    Initial commit

---
+++ src/main/java/net/echonyx/vilplusmod/VilPlusMod.java
@@ -0,0 +1,15 @@
+package net.echonyx.vilplusmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VilPlusMod implements ModInitializer {
+	public static final String MOD_ID = "vilplusmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/echonyx/vilplusmod/VilPlusModClient.java
@@ -0,0 +1,10 @@
+package net.echonyx.vilplusmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VilPlusModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
